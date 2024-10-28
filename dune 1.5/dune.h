75629896637d4835f7ca949ee501a60186ac0365
stayineverywhere (Yun Joo-Young) <yunjooyoung13@gmail.com>
2024-10-29 00:48:44 +0900

    키 타입 정의 , 방향키 입력 받기 ,스켈레톤 코드

--- dune 1.5/dune.h
+++ dune 1.5/dune.h
@@ -3,6 +3,25 @@
 #include <Windows.h>
 #include <conio.h>
 
+#define N_LAYER 2
+#define MAP_WIDTH 80
+#define MAP_HEIGHT 18
+#define MAX_OBJECT 20
+
+#define TICK 10
+
+extern char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH];
+
+typedef struct {
+	int row, column;
+} POSITION;
+
+typedef struct {
+	POSITION pos;
+	CHAR_INFO charInfo[4];
+	int size;
+} CURSOR;
+
 typedef enum {
 	k_none = 0, k_escape, k_space, k_A, k_B, k_C, k_D, k_E, k_F, k_G,
 	k_H, k_I, k_J, k_L, k_M, k_N,
@@ -16,3 +35,26 @@ typedef enum {
 	d_stay = 0,
 	d_up, d_right, d_down, d_left
 } DIRECTION;
+
+typedef struct {
+	int spice; 			// 현재 보유한 스파이스
+	int spice_max; 		// 스파이스 최대 저장량
+	int population; 	// 현재 인구 수
+	int population_max; // 수용 가능한 인구 수
+} RESOURCE;
+
+typedef struct {
+	POSITION pos;		// 현재 위치 (position)
+	POSITION dest;		// 목적지 (destination)
+	int size;			// 크기
+	char repr;			// 화면에 표시할 문자 (representation)
+	int  move_period;	// 몇 ms 마다 한 칸 움직이는지 를 뜻함
+	int  next_move_time;	// 다음에 움직일 시간
+	int nblock;			// sand word의 경우, block수가 증가할 수 있음. 그때 사용
+	POSITION* block;
+} OBJECT;
+
+typedef struct _objects {
+	int layer;
+	OBJECT* obj;
+} OBJECT_POOL;
