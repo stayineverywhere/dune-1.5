abf320a790664f8074a85f85de4d0c65ef2240f8
stayineverywhere (Yun Joo-Young) <yunjooyoung13@gmail.com>
2024-10-30 00:01:12 +0900

    engine.c 에 main() 함수 작성 , 시작화면 , 종료화면을 main() 함수안에 작성

--- dune 1.5/engine.c
+++ dune 1.5/engine.c
@@ -1,3 +1,33 @@
 #include <stdio.h>
 #include "dune.h"
 
+char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH] = { 0 };
+RESOURCE resource = { 0 };
+CURSOR cursor = { 0 };
+
+int main() {
+    KEY oldKey = k_none;
+    int quit = FALSE;
+    int prev_selected; // 선택 객체가 변경되었음을 알려줌
+
+    initBuffers();
+
+    // initBuffers에서 스크린 버터를 가져오거 생성하기 때문에, cursorOff는 initBuffers 다음에 위치하여야 함
+    cursorOff();
+
+    // 시작 화면
+    Intro();
+
+    add_system_message("프로그램을 시작합니다.");
+    add_system_message("Dune Game programmed by Jooyoung Yun, 2024/11/05");
+
+    // 3초 후에 화면을 삭제하고 프로그램 종료
+    Sleep(3000);
+    clearConsole();
+
+    // 종료화면
+    Outtro();
+    cursorOn();
+
+    return 0;
+}
